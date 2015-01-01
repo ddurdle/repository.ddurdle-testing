b9c74525dd06135593cba275afc775d00644fb99
durdle <durdle@debian-i386>
2015-01-01 18:24:57 -0500

    update gdrive name to testing

--- sync-gdrive.sh
+++ sync-gdrive.sh
@@ -1,2 +1,1 @@
-rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive-0.3-dev/* repo/plugin.video.gdrive
-rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive-0.3-dev/* repo.princeoliver/plugin.video.gdrive
+rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive-0.3-dev/* repo/plugin.video.gdrive-testing
